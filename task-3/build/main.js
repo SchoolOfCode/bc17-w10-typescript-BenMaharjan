39848f0e8643291ec919fdcb52f2975d871590c1
Ben Maharjan <benmaharjan@hotmail.co.uk>
2024-09-07 23:27:12 +0100

    task 3

--- task-3/build/main.js
+++ task-3/build/main.js
@@ -1,5 +1,36 @@
 "use strict";
-document.getElementsByClassName("getJoke")[0].addEventListener("click", addText);
-function addText() {
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const joke = document.getElementsByClassName("getJoke")[0].addEventListener("click", getJoke);
+// test function
+function test() {
     alert("button clicked");
+}
+//create a fetch request function
+function getJoke() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield fetch('https://icanhazdadjoke.com/', {
+                headers: {
+                    'Accept': 'application/json'
+                }
+            });
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const data = yield response.json();
+            console.log('Joke:', data.joke);
+            document.getElementsByClassName("dadJoke")[0].innerHTML = data.joke;
+        }
+        catch (error) {
+            console.error('There was a problem fetching the joke:', error);
+        }
+    });
 }
--- task-3/src/main.ts
+++ task-3/src/main.ts
@@ -1,6 +1,35 @@
 
-document.getElementsByClassName("getJoke")[0].addEventListener("click", addText)
+document.getElementsByClassName("getJoke")[0].addEventListener("click", getJoke)
 
-function addText() {
+// test function
+function test() {
     alert("button clicked");
 }
+
+//create a fetch request function
+
+async function getJoke() {
+    try {
+      const response = await fetch('https://icanhazdadjoke.com/', {
+        headers: {
+          'Accept': 'application/json'
+        }
+      });
+      
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      
+      const data = await response.json();
+
+      console.log('Joke:', data.joke);
+      
+    document.getElementsByClassName("dadJoke")[0].innerHTML= data.joke;
+
+    } catch (error) {
+      console.error('There was a problem fetching the joke:', error);
+    }
+  }
+
+  
+    
